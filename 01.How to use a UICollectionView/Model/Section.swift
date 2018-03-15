0aef5bc48dabe974fa974e769c13e972d1c89776
Corotata <corotata@qq.com>
2018-03-15 12:01:59 +0800

    delete some code

---
+++ 01.How to use a UICollectionView/Model/Section.swift
@@ -0,0 +1,13 @@
+//
+//  Section.swift
+//  01.How to use a UICollectionView
+//
+//  Created by Corotata on 2018/3/15.
+//  Copyright © 2018年 Corotata. All rights reserved.
+//
+
+import UIKit
+
+class Section: NSObject {
+
+}
